8f0408deb1c49244b24c11d55ae89953dee70808
TomValent <74118015+TomValent@users.noreply.github.com>
2021-05-20 20:36:05 +0200

    FINAL

--- htab_lookup_add.c
+++ htab_lookup_add.c
@@ -7,65 +7,44 @@
 #include "htab.h"
 #include "htab_struct.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-htab_pair_t* htab_lookup_add(htab_t *table, htab_key_t key)
+htab_pair_t *htab_lookup_add(htab_t *table, htab_key_t key)
 {
-    size_t hash = htab_hash_function(key);
-    size_t index = hash % table->arr_size;
-    struct htab_item *item = table->arr[index];
-
-    if (item == NULL)                                                    //tabulka je prazdna, vytvorim prvy prvok
+    struct htab_item *newItem;
+    newItem = malloc(sizeof(struct htab_item));
+    if (newItem == NULL)
     {
-        struct htab_item *newItem = malloc(sizeof(struct htab_item));
-        if(newItem->pair.key == NULL)
-            return NULL;
-        newItem->pair.key = malloc((strlen(key) + 1) * sizeof(char));    //alokacia pamati pre key a jeho presun
-        if(newItem->pair.key == NULL)
-            return NULL;
-        memcpy((char *)newItem->pair.key, key, (strlen(key) + 1));
-        newItem->next = NULL;
-        newItem->pair.value = 0;
-
-        table->size++;
-        table->arr[index] = newItem;                 //pridani do tabulky
-        return &(item->pair);
-    }
-
-    while (item->next != NULL)                       //ine polozky
-    {
-        if (strlen(key) == strlen(item->pair.key))
-        {
-            if (strncmp(key, item->pair.key, strlen(key)) == 0)
-            {
-                return &(item->pair);
-            }
-        }
-        item = item->next;
-    }
-
-    if (strlen(key) == strlen(item->pair.key))            //posledna polozka
-    {
-        if (strncmp(key, item->pair.key, strlen(key)) == 0)
-        {
-            return &(item->pair);
-        }
-    }
-
-    struct htab_item *newItem = malloc(sizeof(struct htab_item));
-    if(newItem == NULL)
-    {
+        fprintf(stderr, "Chyba pri alokacii pamate\n");
+        free(newItem);
         return NULL;
     }
 
-    newItem->pair.key = malloc((strlen(key) + 1) * sizeof(char));    //alokacia pamati pre key a jeho presun
-    if(newItem->pair.key == NULL)
+    newItem->pair.key = malloc(sizeof(char) * (strlen(key) + 1));
+    if (newItem->pair.key == NULL)
+    {
+        free(newItem);
         return NULL;
-    memcpy((char *)newItem->pair.key, key, (strlen(key) + 1));
+    }
+
+    strcpy((char *)newItem->pair.key, key);
     newItem->next = NULL;
-    newItem->pair.value = 0;
+    size_t hash = (htab_hash_function(key) % table->arr_size);
 
-    newItem->next = newItem;
+    if (table->arr[hash] == NULL)
+    {
+        table->arr[hash] = newItem;
+    }
+    else
+    {
+        struct htab_item *ptr;
+        ptr = table->arr[hash];
+        while (ptr->next != NULL) {
+            ptr = ptr->next;
+        }
+        ptr->next = newItem;
+    }
+
     table->size++;
-
-    return &(newItem->pair);
+    return &newItem->pair;
 }
